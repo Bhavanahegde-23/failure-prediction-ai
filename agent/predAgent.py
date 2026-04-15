2c1f223aaeda267992ec23fff9a1eddf05f69240
Bhavana <holladebhavana23@gmail.com>
2026-04-16 00:17:16 +0530

    Added langgraph agent for the workflow

--- agent/predAgent.py
+++ agent/predAgent.py
@@ -1,8 +1,28 @@
 from langgraph.graph import StateGraph
 import requests
+import joblib
+import pandas as pd
+from langchain_ollama.chat_models import ChatOllama
+from typing import TypedDict
 
+#url for model prediction
 API_URL = "http://127.0.0.1:8000/predict"
 
+#load the model
+model = joblib.load("C:/Users/holla/PycharmProjects/PythonProject1/failure-prediction-ai/model/failure_model.pkl")
+col = joblib.load("C:/Users/holla/PycharmProjects/PythonProject1/failure-prediction-ai/model/columns.pkl")
+
+#Adding llm
+llm = ChatOllama(model="llama3")
+
+#Failure
+failure_map = {
+    "TWF": "Tool Wear Failure",
+    "HDF": "Heat Dissipation Issue",
+    "PWF": "Power Failure",
+    "OSF": "Overstrain Failure",
+    "RNF": "Random Failure"
+}
 #define state
 from typing import TypedDict
 
@@ -10,7 +30,13 @@ class State(TypedDict):
     input: dict
     prob: float
     action: str
+    root_cause : str
+    explain : str
+    report : str
+    summary:str
+
 #Get prediction
+#TODO: add the confidence
 def predict_node(state):
     data = state["input"]
 
@@ -23,6 +49,7 @@ def predict_node(state):
     return state
 
 # Step 2: Decision node
+# Decision node
 def decision_node(state):
     prob = state["prob"]
 
@@ -34,77 +61,109 @@ def decision_node(state):
         state["action"] = "moderate"
     else:
         state["action"] = "safe"
-
     return state
 
-# Step 3: Action node
+# Action node
 def action_node(state):
-    action = state["action"]
 
-    if action == "critical":
-        print("🚨 CRITICAL → Restarting system")
-    elif action == "warning":
-        print("⚠️ Warning → Sending alert")
-
-    elif action == "moderate":
-        print("📊 MODERATE RISK → Monitoring closely")
-    else:
-        print("✅ System stable")
-
+    print("\n================ FINAL REPORT ================\n")
+    print(f"Probability: {round(state['prob'], 2)}")
+    print(f"Summary: {state["summary"]}")
+    print("\n Root Cause:")
+    for rc in state["root_cause"]:
+        print(f" - {rc}")
+    print("\n Explanation:")
+    print(state["explain"])
+    print(f"\n Risk Level: {state['action'].upper()}")
+    print("\n=============================================\n")
     return state
 
-# from langchain.chat_models import ChatOpenAI
-#
-# llm = ChatOpenAI()
-#
-# def diagnose_node(state):
-#     prob = state["prob"]
-#
-#     explanation = llm.invoke(
-#         f"System failure probability is {prob}. Explain cause."
-#     )
-#
-#     print("🧠 Diagnosis:", explanation)
-#     return state
+#explaination node
+def diagnose_node(state):
+    prob = state["prob"]
+    rc = state["root_cause"]
+    prompt = f"""
+    You are an AI system monitoring machine health.
+
+    Current state:
+    - Failure probability: {prob}
+    - Root causes: {rc}
+    - Input data: {state["input"]}
+    - Risk level: {state["action"]}
+
+    Instructions:
+    - DO NOT repeat root cause names directly
+    - Explain using actual sensor values
+    - Be concise (max 120 words)
+    - Give ONE actionable recommendation
+
+    Output format:
+    - Why risky
+    - What could happen
+    - Recommendation
+    """
+
+    response = llm.invoke(prompt)
+
+    state["report"] = {
+    "probability": prob,
+    "risk_level": state["action"],
+    "root_cause": state["root_cause"],
+    "input": state["input"]
+     }
+    summary = f"{state['action'].upper()} risk due to {', '.join(state['root_cause'][:2])}"
+    state["summary"] = summary
+    state["explain"] = response.content
+    return state
+
+#root cause analysis node
+def root_cause_node(state):
+    data = state["input"]
+    # Convert to DataFrame
+    df = pd.DataFrame([data])
+    df = pd.get_dummies(df)
+    df = df.reindex(columns=col, fill_value=0)
+    # Get feature importance
+    importance = model.feature_importances_
+    feature_impact = dict(zip(col, importance))
+    # Get top 3 contributing features
+    top_features = sorted(feature_impact.items(), key=lambda x: x[1], reverse=True)[:3]
+    mapped = [failure_map.get(f, f) for f, _ in top_features]
+    state["root_cause"] = mapped
+    return state
+
 # def root_cause_node(state):
 #     data = state["input"]
 #
-#     # Convert to DataFrame
-#     import pandas as pd
-#     df = pd.DataFrame([data])
-#     df = pd.get_dummies(df)
-#     df = df.reindex(columns=columns, fill_value=0)
+#     # pick top risky features based on value (simple logic)
+#     risky = []
 #
-#     # Get feature importance
-#     importance = model.feature_importances_
+#     if data["Torque [Nm]"] > 60:
+#         risky.append("High Torque")
 #
-#     feature_impact = dict(zip(columns, importance))
+#     if data["Tool wear [min]"] > 200:
+#         risky.append("High Tool Wear")
 #
-#     # Get top 3 contributing features
-#     top_features = sorted(feature_impact.items(), key=lambda x: x[1], reverse=True)[:3]
+#     if data["Rotational speed [rpm]"] > 2500:
+#         risky.append("High Speed")
 #
-#     print("🧠 Root Cause Analysis:")
-#     for f, val in top_features:
-#         print(f" - {f}")
+#     print(" Root Cause:", risky)
 #
-#     state["root_cause"] = top_features
+#     state["root_cause"] = risky
 #     return state
-# builder.add_node("root_cause", root_cause_node)
-#
-# builder.add_edge("predict", "root_cause")
-# builder.add_edge("root_cause", "decision")
+
 # Build graph
 builder = StateGraph(State)
-
 builder.add_node("predict", predict_node)
+builder.add_node("root_cause", root_cause_node)
 builder.add_node("decision", decision_node)
+builder.add_node("explain", diagnose_node)
 builder.add_node("action", action_node)
-
 builder.set_entry_point("predict")
-
-builder.add_edge("predict", "decision")
-builder.add_edge("decision", "action")
-
+builder.add_edge("predict", "root_cause")
+builder.add_edge("root_cause", "decision")
+builder.add_edge("decision", "explain")
+builder.add_edge("explain", "action")
 graph = builder.compile()
 
 # Run
